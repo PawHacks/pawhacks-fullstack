3cc855f01f2f9d549ca689788ddf7fec32f7bd6b
William Pan <williamlpan22@gmail.com>
2024-01-23 00:26:18 -0800

    testing google callback

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,23 +19,56 @@ exports.googleCallback = (req, res, next) => {
         req.login(user, function(err) {
             if (err) { return next(err); }
             const google_id = req.user.google_id; 
-            let query = ` 
-            SELECT * 
-            FROM USERS 
-            WHERE google_id = ?
-            `; 
+            let query = `SELECT * FROM USERS WHERE google_id = ?`; 
 
             connection.query(query, [google_id], (err, result) => { 
-                const user = result
-                if(Object.values(user).some(value => value === null || value === undefined)) { 
-                    return res.redirect('/application')
-                } else { 
-                    return res.redirect('/create_team')
+                if (err) {
+                    console.error("Database query error:", err);
+                    return next(err); // Handle the error appropriately
                 }
-            })
+
+                if (result.length === 0) {
+                    // No user found, handle this case appropriately
+                    return res.redirect('/some-appropriate-url');
+                }
+
+                const user = result[0]; 
+                if(Object.values(user).some(value => value === null || value === undefined)) { 
+                    return res.redirect('/application');
+                } else { 
+                    return res.redirect('/create_team');
+                }
+            });
         });
     })(req, res, next);
 };
+
+
+// exports.googleCallback = (req, res, next) => {
+//     passport.authenticate('google', function(err, user) {
+//         if (err) { return next(err); }
+//         if (!user) { return res.redirect('/login'); }
+
+//         req.login(user, function(err) {
+//             if (err) { return next(err); }
+//             const google_id = req.user.google_id; 
+//             let query = ` 
+//             SELECT * 
+//             FROM USERS 
+//             WHERE google_id = ?
+//             `; 
+
+//             connection.query(query, [google_id], (err, result) => { 
+//                 const user = result
+//                 if(Object.values(user).some(value => value === null || value === undefined)) { 
+//                     return res.redirect('/application')
+//                 } else { 
+//                     return res.redirect('/create_team')
+//                 }
+//             })
+//         });
+//     })(req, res, next);
+// };
 
 exports.protectedRoute = (req, res) => {
     if (req.isAuthenticated()) {
